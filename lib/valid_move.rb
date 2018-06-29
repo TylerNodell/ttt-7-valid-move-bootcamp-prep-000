c2ee200ed318bd306cbadd7760ff9cba8b20372b
TylerNodell <tyler.l.nodell@gmail.com>
2018-06-29 15:14:14 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,17 @@
 # code your #valid_move? method here
-
+def valid_move(board, index)
+  if position_taken?(ind) == false
+    return true
+  elsif position_taken?(ind) == true
+    return false
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(ind)
+  if ind == "X" || ind == "O"
+    return true
+  elsif ind == " " || ind == "" || ind == nil
+    return false
+  end
+end
