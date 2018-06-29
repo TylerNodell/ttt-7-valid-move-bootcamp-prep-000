9b36cf714f08de2aa1c6f9c06fba3a26914577e2
TylerNodell <tyler.l.nodell@gmail.com>
2018-06-29 15:26:35 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if position_taken?(board[index]) == false
     return true
-  elsif position_taken?(board[index]) == true && 0 <= index <= 8
+  elsif position_taken?(board[index]) == true && index.between?(0, 8)
     return false
   end
 end
